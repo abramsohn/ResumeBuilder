9a0dc2e71e9d96fac2b05524ac0358b63d673f57
James Rodriguez <JamesR94@macbook-pro.myfiosgateway.com>
2021-03-15 14:33:32 -0400

    schema update

--- ResumeBuilder-api/server.js
+++ ResumeBuilder-api/server.js
@@ -2,23 +2,50 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const PORT = 3003;
+const cors = require('cors')
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:3001', 'http://localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//       if (whitelist.indexOf(origin) !== -1) {
+//         callback(null, true)
+//       } else {
+//         callback(new Error('Not allowed by CORS'))
+//       }
+//     }
+//   }
+
+app.use(cors(corsOptions))
 
 
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'));
 mongoose.connection.on('disconnected', () => console.log ('mongo is disconnected'));
 
-mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/resumes', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
-  console.log('connected to mongoose...');
+  console.log('connected to mongoose');
 });
 
-app.get('/', (req,res) => {
-    res.send('hello')
-})
+// app.get('/', (req,res) => {
+//     res.send('hello')
+// })
+
+const resumesController = require('./controllers/resume.js')
+app.use('/resumes', resumesController)
 
 
 app.listen(PORT, ()=> {
-    console.log('celebrations happening on port', PORT);
+    console.log('running on port', PORT);
   });
