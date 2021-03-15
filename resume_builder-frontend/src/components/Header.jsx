9a65fa980445f0b6f27fa5a755b9d801044a2a46
Ido Abramsohn <ido.abramsohn@gmail.com>
2021-03-15 14:02:39 -0400

    add hard coded master resume

--- resume_builder-frontend/src/components/Header.jsx
+++ resume_builder-frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends Component{
     constructor(props) {
         super(props)
         this.state = {
-            masterDropdown: true,
+            masterDropdown: false,
         }
     }
 
